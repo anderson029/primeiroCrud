e3581e33e47801d8d70d701139bcc0c244c3958a
anderson029 <andersonfoliveira@yahoo.com>
2024-04-15 12:08:21 -0300

    add service/controller

--- src/main/java/com/nelioalves/primeiroCrud/service/UserService.java
+++ src/main/java/com/nelioalves/primeiroCrud/service/UserService.java
@@ -1,22 +1,41 @@
 package com.nelioalves.primeiroCrud.service;
 
+import com.nelioalves.primeiroCrud.dto.UserDto;
 import com.nelioalves.primeiroCrud.entities.User;
 import com.nelioalves.primeiroCrud.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
-
     @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    private UserRepository userRepository; //importar automaticamente minha interface
+
+    public User createUser(User user){
+        User pessoa = userRepository.save(user);
+        return pessoa;
+    }
+    public UserDto findById(Long id) {
+        User entity = userRepository.findById(id).get();
+        UserDto dto = new UserDto(entity);
+        return dto;
     }
 
-    public User createUser(User user) {
-        return userRepository.save(user);
+    public List<UserDto> findAll(){
+        List<User> listUsuario = userRepository.findAll();
+        List<UserDto> dtos = new ArrayList<>();
+        for (User user : listUsuario) {
+            UserDto dto = new UserDto(user);
+            dtos.add(dto);
+        }
+        return dtos;
     }
+
 }
+
+
 
